545f021239fa722b4c0abfb7b1e88e0558bcde99
florian bracke <florianbracke@gmail.com>
2022-07-12 16:46:04 +0200

    added files

---
+++ index.php
@@ -0,0 +1,57 @@
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+
+        <title>Langory</title>
+
+    </head>
+
+    <body>
+
+        <div class="wrapper">
+
+            <div class="sub-wrapper-one">
+
+                <div class="new">
+
+                    <div class="new-word">
+
+                        <input type="text" name='new-word'>
+
+                    </div>
+
+                    <div class="new-traslation">
+
+                        <input type="text" name='new-traslation'>
+                        <input type="submit" name='new-submit'>
+
+                    </div>
+
+
+
+                </div>
+
+                <div class="select">
+
+                </div>
+
+            </div>
+
+            <div class="sub-wrapper-two">
+
+                <div class="practice">
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+
